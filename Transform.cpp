67e2e5c98de4780d9422372746268441692ce103
Paul H. Hargrove <PHHargrove@lbl.gov>
2014-02-24 18:46:13 -0800

    Refine code as requested by Gary
    This commit adds logic to detect whether or not a given transaltion
    unit contains any instances of the Expr nodes for __builtin_va_arg or
    __builtin_offsetof.  Only if these were seen are the matching defines
    added to the boilerplate at the top of each .trans.c.

--- Transform.cpp
+++ Transform.cpp
@@ -410,6 +410,9 @@ namespace {
 
   class RemoveUPCTransform : public clang::TreeTransform<RemoveUPCTransform> {
     typedef TreeTransform<RemoveUPCTransform> TreeTransformUPC;
+  private:
+    bool haveOffsetOf;
+    bool haveVAArg;
   public:
     RemoveUPCTransform(Sema& S, UPCRDecls* D, const std::string& fileid)
       : TreeTransformUPC(S), AnonRecordID(0), Decls(D), FileString(fileid) {
@@ -429,6 +432,18 @@ namespace {
       UPCSystemHeaders.insert("pupc.h");
 
       UPCHeaderRenames["upc_types.h"] = "upcr_preinclude/upc_types.h";
+
+      haveOffsetOf = haveVAArg = false;
+    }
+    bool HaveOffsetOf() { return haveOffsetOf; }
+    ExprResult TransformOffsetOfExpr(OffsetOfExpr *E) {
+      haveOffsetOf = true;
+      return TreeTransformUPC::TransformOffsetOfExpr(E);
+    }
+    bool HaveVAArg() { return haveVAArg; }
+    ExprResult TransformVAArgExpr(VAArgExpr *E) {
+      haveVAArg = true;
+      return TreeTransformUPC::TransformVAArgExpr(E);
     }
     bool AlwaysRebuild() { return true; }
     ExprResult BuildParens(Expr * E) {
@@ -1885,13 +1900,20 @@ namespace {
       Trans.PrintIncludes(OS);
 
       OS << "#ifndef UPCR_TRANS_EXTRA_INCL\n"
-	"#define UPCR_TRANS_EXTRA_INCL\n"
-	"#ifndef __builtin_va_arg\n" // subclass of Expr - cannot be renamed directly
-	"#define __builtin_va_arg(_a1,_a2) va_arg(_a1,_a2)\n"
-	"#endif\n"
-	"#ifndef __builtin_offsetof\n" // subclass of Expr - cannot be renamed directly
-	"#define __builtin_offsetof(_a1,_a2) offsetof(_a1,_a2)\n"
-	"#endif\n"
+	"#define UPCR_TRANS_EXTRA_INCL\n";
+      if (Trans.HaveVAArg()) { // subclass of Expr - cannot be renamed directly
+        OS <<
+	  "#ifndef __builtin_va_arg\n"
+	  "#define __builtin_va_arg(_a1,_a2) va_arg(_a1,_a2)\n"
+	  "#endif\n";
+      }
+      if (Trans.HaveOffsetOf()) { // subclass of Expr - cannot be renamed directly
+        OS <<
+	  "#ifndef __builtin_offsetof\n"
+	  "#define __builtin_offsetof(_a1,_a2) offsetof(_a1,_a2)\n"
+	  "#endif\n";
+      }
+      OS <<
 	"int32_t UPCR_TLD_DEFINE_TENTATIVE(upcrt_forall_control, 4, 4);\n"
 	"#ifndef UPCR_EXIT_FUNCTION\n"
 	"#define UPCR_EXIT_FUNCTION() ((void)0)\n"
